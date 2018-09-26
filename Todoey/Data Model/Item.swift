9842084ff7390994ad2c0d29dbd01d177eaf05a1
mgable1995 <mgable1995@gmail.com>
2018-09-26 17:50:12 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Michael Gable on 9/26/18.
+//  Copyright © 2018 Michael Gable. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
